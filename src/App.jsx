5eb4864b6238b6430c19d5db3d6828f659049770
Omar Hasan <Omarhasan9204@gmail.com>
2025-11-01 15:02:12 +0100

    first commit react-course

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Greeting from './component/Greeting';
+import Header from './component/Header'
+function App() {
+   return (
+     <>
+       <Greeting name = "Omar" lastname= "Hasan"/>
+       <Header />
+       <Header />
+       <Header />
+     </>
+   );
+}
+
+export default App
